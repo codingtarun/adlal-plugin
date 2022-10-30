b7641ae641362bf633f1302aceb136515762df9c
Tarun Chauhan <coding.tarun@gmail.com>
2022-10-30 21:32:01 +0530

    short_code

--- templates/slider.php
+++ templates/slider.php
@@ -1,8 +1,28 @@
-<h1>SLIDER</h1>
+<?php
 
-<p> 
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Labore animi voluptatum dolorem sapiente, soluta eius quas
-    accusantium fugiat voluptatem rem eligendi saepe facilis 
-    recusandae magnam cumque. Exercitationem dignissimos quia perspiciatis.
-</p>
+
+$args = array(
+    'post_type' => 'slider',
+    'post_status' => 'publish',
+    'posts_per_page' => 1
+); 
+
+$query = new WP_Query($args);
+
+$links = '';
+$title = '';
+
+if($query->have_posts()){
+    while($query->have_posts()){
+        $query->the_post();
+        $link_ = get_post_custom(get_the_ID());
+        print_r($link_);
+        echo "HELLO-->".$link_."<--HII";
+        foreach($link_ as $key => $value){
+            echo $key.' => '.$value;
+        }
+        $title .= get_the_title();
+    }
+}
+
+echo $title."-".$links;
